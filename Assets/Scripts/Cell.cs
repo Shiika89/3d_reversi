93333e9f4a1d8216e85f85d01e246a98c352e0c3
shiika89 <kazuki_plant@yahoo.co.jp>
2021-06-29 18:59:04 +0900

    スクリプトを少し変更

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum CellState
+{
+    None = 0,
+    White = 1,
+    Black = 2
+}
+
+public class Cell : MonoBehaviour
+{
+    [SerializeField] private CellState m_cellState = CellState.None;
+    public CellState CellState
+    {
+        get => m_cellState;
+        set
+        {
+            m_cellState = value;
+        }
+    }
+
+    private void OnValidate()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
